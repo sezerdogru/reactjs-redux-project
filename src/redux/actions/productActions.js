6690e3004e13bdebce1ece2cafb3c364af8002a9
sezerdogru <sezer.dogru91@gmail.com>
2022-06-06 00:08:10 +0300

    Add product add and delete

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,14 +1,68 @@
+
 import * as actionTypes from "./actionTypes"
 
 export function getProductsSuccess(products) {
     return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products }
 }
 
+export function createProductSuccess(product) {
+    return {
+        type: actionTypes.CREATE_PRODUCT_SUCCESS,
+        payload: product
+    }
+}
+
+export function updateProductSuccess(product) {
+    return {
+        type: actionTypes.UPDATE_PRODUCT_SUCCESS,
+        payload: product
+    }
+}
+
+export function saveProductApi(product) {
+    return fetch("http://localhost:3000/products/" + (product.id || ""), {
+        method: product.id ? "PUT" : "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(product)
+    })
+        .then(handleResponse)
+        .catch(handleError)
+}
+
+export function saveProduct(product) {
+    return function (dispatch) {
+        return saveProductApi(product)
+            .then(savedProduct => {
+                product.id
+                    ? dispatch(updateProductSuccess(savedProduct))
+                    : dispatch(createProductSuccess(savedProduct))
+            })
+            .catch(err => {
+                throw err
+            });
+    }
+}
+
+export async function handleResponse(response) {
+    if (response.ok) {
+        return response.json()
+    }
+
+    const error = await response.text
+    throw new Error(error)
+}
+
+export async function handleError(error) {
+    console.log(error)
+    throw error
+}
+
+
 export function getProducts(categoryId) {
     return function (dispath) {
         let url = "http://localhost:3000/products"
-        if(categoryId){
-            url+= "?categoryId="+categoryId
+        if (categoryId) {
+            url += "?categoryId=" + categoryId
         }
         return fetch(url).then(response => response.json())
             .then(result => dispath(getProductsSuccess(result)))
